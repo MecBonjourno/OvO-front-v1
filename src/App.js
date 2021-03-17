e823a721da69ad1c15d56fffb2f3c832a8b17478
mecbonjourno <contatogzii@gmail.com>
2021-03-17 11:21:22 -0300

    troca de maquina

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import './app.css'
+import './App.css'
 import {BrowserRouter, Route} from 'react-router-dom'
 
 import Home from './pages/Home'
--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -3,18 +3,27 @@ import React from 'react'
 
 const Createpost = () => {
     return (
-      <div className="card input-filed">
+      <div className="card input-field" style={{
+        marginTop: "15%",
+        margin: "10px auto",
+        maxWidth: "500px",
+        padding: "20px",
+        textAlign:"center"
+      }}>
           <input type="text" placeholder="title"/>
           <input type="text" placeholder="body"/>
           <div className="file-field input-field"> 
-            <div className="btn">
-                <span>File</span>
+            <div className="btn #757575 grey darken-1">
+                <span>Select Photo</span>
                 <input type="file" />
             </div>
             <div className="file-path-wrapper">
                 <input className="file-path validate" type="text"/>
             </div>
           </div>
+            <button class="btn waves-effect waves-light #757575 grey darken-1" type="submit" name="action">
+             Post
+            </button>
       </div>
     )
 }
